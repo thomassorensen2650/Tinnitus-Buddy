3a10384abe33b98109d501a2f88f419a557e438b
Thomas Sørensen <thomassorensen2650@gmail.com>
2021-03-03 10:50:04 -0800

    Initial Commit

---
+++ Tinnitus-Buddy/Tinnitus_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinnitus_BuddyApp.swift
+//  Tinnitus-Buddy
+//
+//  Created by Thomas Sorensen on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Tinnitus_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
